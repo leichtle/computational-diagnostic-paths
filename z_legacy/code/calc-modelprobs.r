171590a3a9b188f392bbcd136d0fee8f7520cb21
Ellenberger <I0325777@insel.root.local>
2018-09-11 09:12:16 +0200

    Initial import

---
+++ z_legacy/code/calc-modelprobs.r
@@ -0,0 +1,59 @@
+
+## R function using the .C interface for calculating Rao-Blackwellized estimates of model probabilities
+# NOTE: This function is not optimized and is not recommended for large problems at this time.
+#       If the number of unique models is considerably larger than 2^15 or the number of MCMC
+#       iterations is larger than 300,000 this can be quite time consuming.
+#
+# To use this function: 
+# At the terminal type:" R CMD SHLIB callmodelprobs.c" and press Enter
+# or if using Linux the system command can be used from within R: "system("R CMD SHLIB callmodelprobs.c")"
+
+system("R CMD SHLIB callmodelprobs.c")
+dyn.load("callmodelprobs.so") 
+is.loaded("callmodelprobs") # make sure it is loaded
+
+modelprobs.rb <- function(n.unique,niter,p,gammaunique,probmat.oda)
+  .C("callmodelprobs",
+     as.integer(n.unique),
+     as.integer(niter),
+     as.integer(p),
+     as.integer(gammaunique),
+     as.double(probmat.oda),
+     modelprobs = double(n.unique))$modelprobs
+# Arguments: 
+# n.unique:     Number of unique models for which RB estimates of posterior model probabilities need to be calculated
+# niter:        Number of MCMC iterations for which the posterior inclusion probabilities
+#               will be used in the calculation
+# p:            Number of predictors in the full model
+# gammaunique:  An n.unique by p matrix of unique models (denoted by gamma) for which posterior model probabilities
+#               are to be estimated. 
+# probmat.oda:  An niter by p matrix of posterior inclusion probabilities available from the output of oda.bma,
+#               based on which the Rao-Blackwellized estimates of model probabilities will be calculated
+
+
+# test run for probit regression using Pima Indians diabetes data
+burnin.sim <- 500;
+Gtot <- 1000;
+library(MASS)
+data(Pima.tr)
+data(Pima.te)
+pima <- rbind(Pima.tr,Pima.te)
+dim(pima) # 532 8
+names(pima)
+pimay <- pima$type
+pimay <- as.numeric(pimay);
+pimay[pimay==1] <- 0; pimay[pimay==2] <- 1;
+pimax <- pima[,1:7]
+source("oda.bma.r")
+
+oda.pima <- oda.bma(x=pimax,y=pimay,niter=Gtot,burnin=burnin.sim,lambda=1,model="probit",prior="normal");
+
+# matrix of unique models from ODA
+gamma.u <- unique(oda.pima$gamma[-c(1:burnin.sim),])
+
+# vector of RB estimates of model probabilities corresponding to models in gamma.u
+probest <- modelprobs.rb(n.unique=nrow(gamma.u),niter=nrow(oda.pima$incprob[-c(1:burnin.sim),]),p=ncol(gamma.u),
+         gammaunique=t(gamma.u),probmat.oda=t(oda.pima$incprob[-c(1:burnin.sim),]))
+
+# Estimated posterior probability of unsampled models
+1-sum(probest) 
---
+++ z_legacy/code/oda.bma.r
@@ -0,0 +1,23 @@
+
+
+oda.bma <- function(x,y,niter,burnin,model="lm",prior="Students-t",lambda=1,alpha=1)
+  {
+    source("oda.normal.r")
+    source("oda.studentst.r")
+    source("oda.probit.r")
+    # Error message
+    if (model=="probit" & prior!="normal")
+       stop ("ODA for Probit regression is available with normal priors only at this time \n Please re-run with prior=normal")
+    if (model%in%c("lm","probit")==FALSE)
+      stop ("Please re-run specifying the argument model=lm or model=probit")
+    if (model=="lm" & prior%in%c("normal","Students-t")==FALSE)
+      stop ("ODA for linear regression is available with normal or Students-t prior at this time \n Please re-run with prior=normal or prior=Students-t")
+    # Run for appropriate arguments
+    if (model=="lm" & prior=="normal")
+      result <- oda.normal(xo=x,yo=y,niter=niter,lam.spec=lambda,burnin=burnin)
+    else if (model=="lm" & prior=="Students-t")
+       result <- oda.studentst(xo=x,yo=y,niter=niter,burnin=burnin,alpha=alpha)
+    else if (model=="probit" & prior=="normal")
+       result <- oda.probit(xo=x,zo=y,niter=niter,burnin=burnin,lam.spec=lambda)
+    return(result)
+  }
