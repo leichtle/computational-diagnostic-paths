28642706b0c1651e9e2afef7bf76c61f0191b27e
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2018-10-11 11:34:53 +0200

    Clean up repository

--- model/a_imputation/impute_simple.py
+++ model/a_imputation/impute_simple.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 from sklearn.preprocessing import Imputer
 
 
@@ -18,3 +19,23 @@ def selectively_impute(df):
 
     df = pd.concat([df, imputation_data], axis=1)                               # concatenate dfs again
     return df
+
+
+def impute_missing_data(df):
+
+    print("Performing imputation...")
+    df = selectively_impute(df)  # perform selective imputation
+    print("...Done.")
+
+    print("----------------------")
+    print("Imputed data for inspection:")
+    print(df.head())                 # print data for sanity check
+
+    return df
+
+
+if __name__ == "__main__":
+    mi_df = pd.read_csv('../../data/raw_myocardial_ischemia.csv', header=0)  # read data from csv
+    mi_df = impute_missing_data(mi_df)
+    file_name = './results/' + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '-mi-imputation.csv'
+    mi_df.to_csv(file_name, index=False)
