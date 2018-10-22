9be38a2c718035e60f16e256b3dd3002e8df04f9
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2018-10-22 15:20:41 +0200

    Add fix dataframe script

---
+++ data/raw/myocardial_ischemia_1929_fix_dataframe.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp ./myocardial_ischemia_1929.csv ./myocardial_ischemia_1929_fixed.csv
+
+# add NA between two ; (first two), then add NA at the beginning and end of the line
+sed -i 's/\;\;/\;NA\;/g
+s/\;\;/\;NA\;/g
+s/\;$//g
+s/^\;/NA\;/g
+s/\;N$//g' myocardial_ischemia_1929_fixed.csv
