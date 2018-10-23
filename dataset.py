773e8c12eb91801efaae6f532b89b84c14bf6448
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2018-10-23 10:42:00 +0200

    Implement basic json logging

--- dataset.py
+++ dataset.py
@@ -12,9 +12,11 @@ import os
 import re
 import logging
 
+from src.common.json_logging import setup_logging
 from src.features.labels import add_binary_diagnosis_label
 from src.features.imputations import impute
 
+setup_logging("src/common/logging.json")  # setup logger
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
@@ -30,26 +32,35 @@ if __name__ == "__main__":
     imputation_type = impute.ImputationType[args.imputation_type]
     iteration_qty = args.niter
 
-    logger.info("Loading dataset...")
+    logger.info(str({"message": "NEW DATASET",
+                     "path": dataset_path,
+                     "imputation_type": imputation_type,
+                     "iteration_qty": iteration_qty})
+                )
+
+    logger.info(str({"message": "Load dataset",
+                     "path": dataset_path}))
     mi_df = pd.read_csv(dataset_path, header=0)  # read data from csv
-    logger.info("...Done.")
 
-    logger.info("Adding binary diagnosis label...")
+    logger.info(str({"message": "Add binary diagnosis label"}))
     mi_df = add_binary_diagnosis_label(mi_df)
-    logger.info("...Done.")
 
-    logger.info("Performing imputation")
+    logger.info(str({"message": "Perform imputation",
+                     "imputation_type": imputation_type.name,
+                     "exclude_from_imputation": "[\"HDIA\", \"Klasse\"]",
+                     "iteration_qty": iteration_qty})
+                )
     mi_df = impute.impute_missing_data(mi_df, imputation_type, exclude_from_imputation=["HDIA", "Klasse"], iteration_qty=iteration_qty)
-    logger.info("...Done.")
 
-    logger.info("Writing dataset to file...")
+    # write dataset to file
     file_name = os.path.splitext(os.path.basename(dataset_path))[0]
 
     # prepare dataset store path
     path = 'data/interim/'
     if not re.match("\d\d\d\d\d\d\d\d\d\d\d\d", file_name):  # try to find a timestamp with 4 digit year and each 2 digits for month, day, hour, minute, second
         path += datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '_'
-    path +=  file_name + '_impType_'+imputation_type.name+'_nIter_'+str(iteration_qty)+'_label.csv'
-    logger.info(path)
+    path += file_name + '_impType_'+imputation_type.name+'_nIter_'+str(iteration_qty)+'_label.csv'
+    logger.info(str({"message": "Write dataset to file",
+                     "path": path})
+                )
     mi_df.to_csv(path, index=False)
-    logger.info("...Done.")
