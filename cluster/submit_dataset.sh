156428227611a662f1054384f5e5a4d41fb74810
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2018-10-23 11:04:25 +0200

    Write script for dataset slurm job on HPC cluster

---
+++ cluster/submit_dataset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J dataset_evidence-based-diagnostic-paths
+#SBATCH -o cluster/log/%j_feature_std.log
+#SBATCH -e cluster/log/%j_feature_err.log
+#SBATCH -t 08:00:00
+#SBATCH -p nodes
+#SBATCH -A insel
+#SBATCH --mem-per-cpu=180000
+
+echo $1
+source /home/INSEL/i0325777/miniconda3/bin/activate evidence-based-diagnostic-paths # conda activate evidence-based-diagnostic-paths # source venv3/bin/activate
+python dataset.py $1
+
