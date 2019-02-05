563a503bf1b90f83006c9e2cd81c58ddc93adbc4
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2019-02-05 10:21:28 +0100

    Remove memory constraint from job

--- cluster/submit_dataset_r.sh
+++ cluster/submit_dataset_r.sh
@@ -5,9 +5,8 @@
 #SBATCH -t 700:00:00
 #SBATCH -p nodes
 #SBATCH -A insel
-#SBATCH --mem-per-cpu=180000
 
 echo $1
 export PATH=/software/bin:$PATH # append R location to path before running it
-Rscript ./dataset.r $1  # > ./traces/r_info.log 2> ./traces/r_error.log
+Rscript ./dataset.r $1
 
