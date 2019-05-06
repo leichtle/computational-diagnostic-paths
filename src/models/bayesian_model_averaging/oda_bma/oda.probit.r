71b62c0d957b8cda1fe286ae6e44f97a04323c9f
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2019-05-06 11:02:36 +0200

    Perform linear regression if only one feature is available instead of ridge/lasso (seems to be a limitation of both)

--- src/models/bayesian_model_averaging/oda_bma/oda.probit.r
+++ src/models/bayesian_model_averaging/oda_bma/oda.probit.r
@@ -53,7 +53,7 @@ oda.probit <- function(xo, zo, niter, burnin, lam.spec=1, coeffShrink=0, ridgeLa
                 xogamma <- xo[, gamma == 1]
                 xoxogamma <- xoxo[gamma == 1, gamma == 1]
                 lamgamma <- lam[gamma == 1]
-				if (coeffShrink == 0 || (ridgeLassoBlend != 0 && sum(gamma) <= 1)) {  # if no coefficient shrinking is applied, we keep the original linear model
+				if (coeffShrink == 0 || sum(gamma) <= 1) {  # if no coefficient shrinking is applied, we keep the original linear model
                     # or if lasso is involved and only one feature (algo limitation: https://stackoverflow.com/questions/29231123/why-cant-pass-only-1-coulmn-to-glmnet-when-it-is-possible-in-glm-function-in-r)
 					gamma.lm <- lm(yo ~ xogamma)
 					betaogamma <- gamma.lm$coefficients[-1]
@@ -95,8 +95,8 @@ oda.probit <- function(xo, zo, niter, burnin, lam.spec=1, coeffShrink=0, ridgeLa
             }
             yc <- c(yo, ya)
 
-			if (coeffShrink == 0 || (ridgeLassoBlend != 0 && sum(gamma) <= 1)) {  # if no coefficient shrinking is applied, we keep the original linear model
-                # or if lasso is involved and only one feature (algo limitation: https://stackoverflow.com/questions/29231123/why-cant-pass-only-1-coulmn-to-glmnet-when-it-is-possible-in-glm-function-in-r)
+			if (coeffShrink == 0 || sum(gamma) <= 1) {  # if no coefficient shrinking is applied, we keep the original linear model
+                # or if ridge/lasso is involved and only one feature (algo limitation: https://stackoverflow.com/questions/29231123/why-cant-pass-only-1-coulmn-to-glmnet-when-it-is-possible-in-glm-function-in-r)
 				fullc.lm <- lm(yc ~ xcin - 1)
 				betaolsc[i,] <- as.vector(fullc.lm$coefficients)
 			}
